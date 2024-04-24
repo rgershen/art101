4b9e13287f33625c395bb11406b5e95b6270baff
rgershen <166078653+rgershen@users.noreply.github.com>
2024-04-23 22:10:32 -0700

    lab 4 complete

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,35 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Rory
+// Date: 04/23/24
 
 // Constants
+Rock 
+Paper
+Scissors
 
 // Functions
+// Print the characters of rock paper and scissors
+
+// Print a score table, with a section for each the player and the computer
+
+//Identify variables 
+  //Rock
+  //Paper
+  //Scissors
+
+//Allow Rock to beat Scissors, Paper to beat Rock, and Scissors to beat paper.
+//Allow players to select Rock, Paper or Scissors.
+
+//Confirm Selection
+
+//Allow computer to randomly select Rock, Paper or scissors with a 33% probability for each
+
+//Reveal players selection and computer selection after a 3 second countdown
+
+//Award Points
+  //a. If players selection beats computers selection, increase players point tally +1
+  //b. If computers selection beats players selection, increase computers point tally +1
+  //c. If computers selection is the same as players selection, do not increase either point tally
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
